679401ac8d99293236a7e90386df411993a5d8a9
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-03 14:17:51 +0200

    todolist

---
+++ precache-manifest.6beae8312df8cb662634315cf91e4f88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717a3bd7c35a244e5b02dda7c7b259f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "839a20cab6770ecfdee9",
+    "url": "/static/css/main.e56f2c6b.chunk.css"
+  },
+  {
+    "revision": "3cb376f3e16d7c02850f",
+    "url": "/static/js/2.ce3b91ce.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ce3b91ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "839a20cab6770ecfdee9",
+    "url": "/static/js/main.0dde8638.chunk.js"
+  },
+  {
+    "revision": "39d47c611a20b26e8983",
+    "url": "/static/js/runtime-main.bcabb252.js"
+  }
+]);
